31afe9a9a4c792f0017865b588ca33c9ad42453a
Fierie <denhonchodm0@gmail.com>
2025-12-26 18:51:46 +0700

    Add files via upload

---
+++ thuyet_trinh_1.py
@@ -0,0 +1,34 @@
+import requests
+import plotly.express as px
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+
+response_dict = r.json()
+repo_dicts = response_dict['items']
+
+repo_links, stars, labels = [], [], []
+
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    
+    stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+title = "Most-Starred Python Projects on GitHub"
+labels_dict = {'x': 'Repository', 'y': 'Stars'}
+
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels_dict, hover_name=labels)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
+fig.show()
